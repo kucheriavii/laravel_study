c60cf8ae0e691877e876bebab48022be7bf77baa
r.ostapchuk <kucheriavii@gmail.com>
2024-05-05 20:06:41 +0300

    I try to studing

---
+++ resources/views/contact.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Hello from contact Page!</h1>
+    <div class="contact__preview">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex labore repellendus voluptate. Ad fuga ipsa omnis rerum ut! Ab asperiores distinctio dolore eius est fugiat itaque minus numquam obcaecati pariatur quasi sequi, sit, voluptates. Adipisci assumenda atque culpa delectus dignissimos dolore dolorem doloremque doloribus ea eius eos esse est excepturi fuga fugit in minus molestiae necessitatibus neque nisi, porro praesentium qui recusandae sapiente tenetur ut vitae voluptatibus. Cupiditate dolorum facere fugiat labore, quam qui temporibus. At cum deserunt dolore eaque eos fugit necessitatibus quaerat vel. Consequuntur cupiditate dignissimos fugit in inventore laboriosam magni nulla officia, quos ut. Debitis eligendi illum provident sapiente! Ab ipsam minima officiis! Adipisci aspernatur atque aut consequatur, culpa debitis dicta dolorem doloremque dolores dolorum eos et facere fuga illo in maxime natus nihil numquam officiis, omnis optio praesentium provident quaerat quam quia reiciendis vitae voluptates! Animi consequuntur, cum dicta dolore ducimus facere fugiat id impedit itaque labore nesciunt nisi nobis non odit possimus provident quidem ratione sapiente sint soluta vero vitae voluptates. Aliquam atque blanditiis delectus deleniti dolorem eius error eveniet facilis fugit id inventore iure laborum maxime mollitia natus nulla odio quaerat quisquam quod recusandae rem repellat repellendus sapiente, sed veniam vero voluptatum. Et expedita nulla obcaecati ullam vel! Animi beatae dicta dignissimos distinctio doloremque ea eaque eos, eum fuga inventore iure laboriosam maiores, necessitatibus nihil nisi odit officia, optio quae quaerat quam quis ratione repellat repellendus repudiandae ut velit vero vitae. Commodi dicta distinctio dolorem doloribus eaque eligendi et ex excepturi expedita fugit ipsam iste laboriosam modi nesciunt numquam odio quasi ratione, rem sed tempora tempore velit vitae. At et facere impedit in itaque modi neque repellendus. Asperiores autem dolores doloribus ea eius eum maiores minima non numquam obcaecati porro rem, sint totam. Adipisci animi aspernatur aut culpa cum dolore dolorem ducimus error explicabo facere laborum maiores mollitia nesciunt nisi nobis non odit optio quae quam, quibusdam quis reprehenderit suscipit vel voluptas voluptatum? Blanditiis culpa eligendi fuga illum ipsam natus perspiciatis quibusdam sunt. A accusamus accusantium cum et ex ipsum itaque labore magni modi molestias natus nisi, optio perspiciatis provident sapiente temporibus vel, veritatis! Ab accusamus adipisci aperiam beatae dolor dolorem est explicabo iste labore, minima molestiae numquam odio pariatur quasi sed similique suscipit, voluptatum? Adipisci alias animi aperiam, aut commodi culpa delectus dignissimos dolorum earum esse exercitationem, ipsam iste laboriosam maxime molestias nemo nesciunt omnis, quae quam quos reiciendis saepe sunt vero vitae voluptatibus? Architecto asperiores aut deserunt dolores dolorum error impedit ipsum laudantium provident, quae quasi, sit soluta suscipit tenetur, ut? Accusamus eligendi eum ex exercitationem fugit, laborum maiores nihil officia provident quasi quibusdam soluta ullam voluptatem? Cumque expedita harum laboriosam maxime nulla, quo sed vero vitae voluptatum? A accusantium, amet animi aperiam assumenda beatae cum deserunt dignissimos doloribus est, illum incidunt iste itaque laborum maiores nemo nihil officiis quae quas quibusdam quos reprehenderit repudiandae similique soluta tempora temporibus ullam voluptas! Commodi consectetur eligendi, in itaque perspiciatis praesentium quia ratione tempora ullam veritatis. Alias beatae dolore laboriosam laudantium quae quia voluptas voluptate!
+        </p>
+        <ul>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Addres: Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+            <li>Phone: 0677778899</li>
+            <li>Fax: 123 456 78</li>
+            <li>mail: lorem@ipsum.dolor</li>
+        </ul>
+    </div>
+
+</body>
+</html>
