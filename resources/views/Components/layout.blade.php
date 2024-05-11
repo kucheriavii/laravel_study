e87db3956c17bfe5186a4999da383d1decada4bc
r.ostapchuk <kucheriavii@gmail.com>
2024-05-11 22:03:49 +0300

    test

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -10,7 +10,7 @@
     <body>
         <nav>
             <x-nav-link href="/">Home</x-nav-link>
-            <x-nav-link href="/about">About</x-nav-link>
+            <x-nav-link class="test" href="/about">About</x-nav-link>
             <x-nav-link href="/contacts">Contacts</x-nav-link>
         </nav>
 
